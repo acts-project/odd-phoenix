3446b9ca298e2d3f3f0edb652f066c6cbf6287f3
paulgessinger <hello@paulgessinger.com>
2021-11-22 13:36:27 +0000

    Deploying to gh-pages from @ acts-project/odd-phoenix@e98bf9530c4cab4f233a90058e8cb06df32ae0b6 🚀

--- src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'open-data-detector-root',
-  templateUrl: './app.component.html',
-})
-export class AppComponent {
-  title = 'phoenix-odd';
-}
--- src/app/functions/url-options.ts
+++
@@ -1,3 +0,0 @@
-export const getUrlOptions = () => {
-  return new URLSearchParams(window.location.href.substr(window.location.href.lastIndexOf('?')));
-};
